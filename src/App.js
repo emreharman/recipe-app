b395134a4d52d6baac2d33b70b2ae6a204483542
emreharman <emre.harman@ogr.sakarya.edu.tr>
2021-02-15 02:16:05 +0300

    editing app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  //API Stuff
+  const APP_ID = "daa7e070";
+  const APP_KEY = "2a8b00de2155576da5b9764acef0426a";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
